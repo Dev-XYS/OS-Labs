96b87f38d966ccb0d04289091d88bf4b25a6639c
Yushuo Xiao <xiaoyushuo0419@live.com>
2020-12-04 09:41:05 +0800

    [ex] Lab 4, Exercise 9
    Dispatch page faults to the user-mode handler.

--- kern/trap.c
+++ kern/trap.c
@@ -339,6 +339,40 @@ page_fault_handler(struct Trapframe *tf)
 
 	// LAB 4: Your code here.
 
+	if (curenv->env_pgfault_upcall) {
+		// prepare the UTrapframe
+		struct UTrapframe utf;
+		utf.utf_fault_va = fault_va;
+		utf.utf_err = tf->tf_err;
+		utf.utf_regs = tf->tf_regs;
+		utf.utf_eip = tf->tf_eip;
+		utf.utf_eflags = tf->tf_eflags;
+		utf.utf_esp = tf->tf_esp;
+
+		if (UXSTACKTOP - PGSIZE <= tf->tf_esp && tf->tf_esp < UXSTACKTOP) {
+			// This is a recursive fault.
+
+			// make sure the user exception stack doesn't overflow
+			user_mem_assert(curenv, (void *)tf->tf_esp - 4 - sizeof(struct UTrapframe), 4 + sizeof(struct UTrapframe), 0);
+
+			// setup the trap frame
+			*((uint32_t *)tf->tf_esp - 1) = 0;
+			*((struct UTrapframe *)((uint32_t *)tf->tf_esp - 1) - 1) = utf;
+		}
+		else {
+			// This is non-recursive.
+
+			// no need to check overflow
+
+			// setup the trap frame
+			*((uint32_t *)UXSTACKTOP - 1) = 0;
+			*((struct UTrapframe *)((uint32_t *)UXSTACKTOP - 1) - 1) = utf;
+		}
+
+		// restart the env (execute the page fault handler)
+		env_run(curenv);
+	}
+
 	// Destroy the environment that caused the fault.
 	cprintf("[%08x] user fault va %08x ip %08x\n",
 		curenv->env_id, fault_va, tf->tf_eip);
