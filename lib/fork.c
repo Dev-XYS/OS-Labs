b092518e120825bd3f9c25be269d7bad51418fd1
Yushuo Xiao <xiaoyushuo0419@live.com>
2020-12-05 13:22:49 +0800

    [ex] Lab 4, Exercise 12
    Implement the user-mode `fork`.

--- lib/fork.c
+++ lib/fork.c
@@ -26,6 +26,19 @@ pgfault(struct UTrapframe *utf)
 
 	// LAB 4: Your code here.
 
+	// Should `uvpd` be used?
+	if (!(uvpd[PDX(addr)] & PTE_P) || !(uvpt[PGNUM(addr)] & PTE_P)) {
+		panic("pgfault: The faulting address is not mapped. va = 0x%08x\n", addr);
+	}
+
+	if (!(uvpt[PGNUM(addr)] & PTE_COW)) {
+		panic("pgfault: The faulting access is not to a copy-on-write page. va = 0x%08x\n", addr);
+	}
+
+	if (!(err & FEC_WR)) {
+		panic("pgfault: The faulting access was not a write. va = 0x%08x\n", addr);
+	}
+
 	// Allocate a new page, map it at a temporary location (PFTEMP),
 	// copy the data from the old page to the new page, then move the new
 	// page to the old page's address.
@@ -34,7 +47,25 @@ pgfault(struct UTrapframe *utf)
 
 	// LAB 4: Your code here.
 
-	panic("pgfault not implemented");
+	void *pg = ROUNDDOWN(addr, PGSIZE);
+	void *tmppg = (void *)PFTEMP;
+
+	r = sys_page_alloc(0, tmppg, PTE_U | PTE_W | PTE_P);
+	if (r < 0) {
+		panic("pgfault: sys_page_alloc() failed: %e\n", r);
+	}
+
+	memcpy(tmppg, pg, PGSIZE);
+
+	r = sys_page_map(0, tmppg, 0, pg, PTE_U | PTE_W | PTE_P);
+	if (r < 0) {
+		panic("sys_page_map failed: %e\n", r);
+	}
+
+	r = sys_page_unmap(0, tmppg);
+	if (r < 0) {
+		panic("sys_page_unmap failed: %e\n", r);
+	}
 }
 
 //
@@ -54,7 +85,32 @@ duppage(envid_t envid, unsigned pn)
 	int r;
 
 	// LAB 4: Your code here.
-	panic("duppage not implemented");
+
+	bool t = !!(uvpt[pn] & PTE_COW);
+	void *addr = (void *)(pn * PGSIZE);
+
+	// Lessons learned the hard way: cannot use `thisenv->env_id` to get the
+	// child's envid. Because `thisenv` is not set yet, and the setting will
+	// cause page faults.
+
+	if ((uvpt[pn] & PTE_COW) || (uvpt[pn] & PTE_W)) {
+		r = sys_page_map(0, addr, envid, addr, PTE_COW | PTE_U | PTE_P);
+		if (r < 0) {
+			return r;
+		}
+
+		r = sys_page_map(0, addr, 0, addr, PTE_COW | PTE_U | PTE_P);
+		if (r < 0) {
+			return r;
+		}
+	}
+	else {
+		r = sys_page_map(0, addr, envid, addr, PTE_U | PTE_P);
+		if (r < 0) {
+			return r;
+		}
+	}
+
 	return 0;
 }
 
@@ -78,7 +134,70 @@ envid_t
 fork(void)
 {
 	// LAB 4: Your code here.
-	panic("fork not implemented");
+
+	int r;
+
+	set_pgfault_handler(pgfault);
+
+	envid_t eid = sys_exofork();
+
+	if (eid < 0) {
+		return eid;
+	}
+
+	if (eid) {
+		// parent
+
+		// duplicate the page table
+		for (int pdn = 0; pdn < PDX(UTOP); pdn++) {
+			if (uvpd[pdn] & (PTE_U | PTE_P)) {
+				for (int ptn = 0; ptn < NPTENTRIES; ptn++) {
+					int pn = pdn * NPTENTRIES + ptn;
+
+					// skip the user kernel stack page
+					if (pn == PGNUM(UXSTACKTOP - PGSIZE)) continue;
+
+					if (uvpt[pn] & (PTE_U | PTE_P)) {
+						int r = duppage(eid, pn);
+						if (r < 0) {
+							cprintf("duppage error : %e\n", r);
+						}
+					}
+				}
+			}
+		}
+
+		// allocate the user exception stack
+		r = sys_page_alloc(eid, (void *)UXSTACKTOP - PGSIZE, PTE_U | PTE_W | PTE_P);
+		if (r < 0) {
+			return r;
+		}
+
+		// set the page fault handler entrypoint for the child
+		// (_pgfault_handler copied (or shared), but `env_pgfault_upcall` is in the kernel)
+		extern void _pgfault_upcall(void);
+		r = sys_env_set_pgfault_upcall(eid, _pgfault_upcall);
+		if (r < 0) {
+			return r;
+		}
+
+		// mark the child as runnable
+		r = sys_env_set_status(eid, ENV_RUNNABLE);
+		if (r < 0) {
+			return r;
+		}
+
+		return eid;
+	}
+	else {
+		// child
+
+		envid_t envid = sys_getenvid();
+		thisenv = &envs[ENVX(envid)];
+
+		return 0;
+	}
+
 }
 
 // Challenge!
